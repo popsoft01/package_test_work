2751b7b6eee1708be2cc7f32ad802b675f9d7813
popsoft01 <popoolatunde52@gmail.com>
2021-06-07 02:52:54 +0100

    new push

--- TEST/com/ChapterSeven/AirlineReservationClient.java
+++ TEST/com/ChapterSeven/AirlineReservationClient.java
@@ -1,12 +1,29 @@
 package com.ChapterSeven;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AirlineReservationClient {
+    AirLine airLine;
+
+
+    @BeforeEach
+    void startWith(){
+        airLine = new AirLine();
+    }
     @Test
     void present(){
-//        assertNotNull(AirLine);
+        assertNotNull(airLine);
+    }
+//    @Test
+//    void airlineCanAssignFirstClassSeat(){
+//
+//    }
+
+    @Test
+    void canSelectSeatType(){
+
     }
 }
--- src/com/ChapterSeven/AirLine.java
+++ src/com/ChapterSeven/AirLine.java
@@ -1,4 +1,51 @@
 package com.ChapterSeven;
 
 public class AirLine {
+    private static int ECONOMIC_CLASS_SEAT = 5;
+    boolean[] seatStatus = new boolean[10];
+    private int FIRST_CLASS_SEAT = 5;
+
+    public boolean assignFirstClass() {
+        int i = 0;
+        if (FIRST_CLASS_SEAT > 0) {
+            for (; i < 5; i++) {
+                if (seatStatus[i] = false) {
+                    seatStatus[i] = true;
+                    FIRST_CLASS_SEAT--;
+                }
+            }
+        }
+
+        return seatStatus[i];
+    }
+
+    public boolean assignEconomicClass() {
+        int i = 0;
+        if (ECONOMIC_CLASS_SEAT > 0) {
+            for (; i < 5; i++) {
+                if (seatStatus[i] = false) {
+                    seatStatus[i] = true;
+                    ECONOMIC_CLASS_SEAT--;
+                }
+            }
+        }
+
+        return seatStatus[i];
+    }
+
+    public void displayGreet() {
+        String greeting = """
+                welcome Afric Airline
+                Enter 1 for first class seat
+                Enter 2 for Ecinomic class seat
+                """;
+        System.out.println(greeting);
+    }
+    public void customerChoice(int seatChoice){
+        if (seatChoice == 1){
+            assignFirstClass();
+        }else if(seatChoice == 2){
+            assignEconomicClass();
+        }
+    }
 }
