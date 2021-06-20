8c4fb1f7fef9a53467ef9299e71790099d515a89
popsoft01 <popoolatunde52@gmail.com>
2021-06-20 16:52:40 +0100

    new push

--- src/com/ChapterEight/TimeInternal.java
+++ src/com/ChapterEight/TimeInternal.java
@@ -7,6 +7,21 @@ public class TimeInternal {
     private int minute;
     private int seconds;
 
+    public TimeInternal(int hour, int minute, int seconds){
+        if (hour > 0 && hour <= 24){
+            this.hour = hour;
+        }else throw new IllegalArgumentException("hour must be between 1 and 24");
+
+        if (minute > 0 && minute <=60){
+            this.minute = minute;
+        }else throw new IllegalArgumentException("Minute must be btw 1 and 60");
+
+        if (seconds > 0 && seconds <=60){
+            this.seconds = minute;
+        }else throw new IllegalArgumentException("Second must be btw 1 and 60");
+
+    }
+
     public void sethour(int hour) {
         if (hour > 0 && hour <= 24){
         this.hour = hour;
