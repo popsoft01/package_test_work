c85079530786914ca285a5605cb8e29be0bb511f
popsoft01 <popoolatunde52@gmail.com>
2021-06-22 06:52:09 +0100

    new push

---
+++ src/chapterSixteen/RepeatWord.java
@@ -0,0 +1,31 @@
+package chapterSixteen;
+
+public class RepeatWord {
+
+        public static void main(String[] args)
+        {
+            String sen = "hi hello hi good morning hello";
+            count(sen);
+
+        }
+
+    private static void count(String sen) {
+        String[] word = sen.split(" ");
+        int count=0;
+        for( int i=0;i<word.length;i++)
+        {
+            for( int j=0;j<word.length;j++)
+            {
+                if(word[i].equals(word[j]))
+                {
+                    count++;
+                }
+                if(count>1)
+                    System.out.println("the word "+word[i]+" occured "+ count+" time");
+            }
+
+        }
+    }
+
+
+}
--- src/chapterSixteen/chapterSixteen.java
+++ src/chapterSixteen/chapterSixteen.java
@@ -1,4 +1,4 @@
-package com;
+package chapterSixteen;
 
 import java.util.Collections;
 import java.util.HashSet;
