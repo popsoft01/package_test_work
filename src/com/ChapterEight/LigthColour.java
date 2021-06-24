ef46beced025328b42383bc7f04c2c7ee16f85c5
popsoft01 <popoolatunde52@gmail.com>
2021-06-24 03:52:34 +0100

    new push

--- src/com/ChapterEight/LigthColour.java
+++ src/com/ChapterEight/LigthColour.java
@@ -1,5 +1,22 @@
 package com.ChapterEight;
 
 public enum LigthColour {
-    RED, GREEN, YELLOW;
+    RED(20),
+    GREEN(30),
+    YELLOW(30);
+
+
+    LigthColour(int duration) {
+    }
+
+    @Override
+    public String toString() {
+        switch (this){
+            case RED -> { return "red"; }
+            case GREEN -> { return "green"; }
+            case YELLOW -> { return "yellow"; }
+
+        }
+        return super.toString();
+    }
 }
--- src/com/ChapterEight/TrafficSignal.java
+++ src/com/ChapterEight/TrafficSignal.java
@@ -1,8 +1,15 @@
 package com.ChapterEight;
 
 public class TrafficSignal {
-    public LigthColour getSignal(int duration) {
-        if ()
+    public Object getSignal(int duration) {
+        if (duration >=40 && duration <= 60)
+            return LigthColour.RED;
+        else if(duration >=20 && duration <= 40)
+            return LigthColour.GREEN;
+        else if(duration >=0 && duration <= 20)
+            return LigthColour.YELLOW;
+        else
+            return null;
     }
 
 }
