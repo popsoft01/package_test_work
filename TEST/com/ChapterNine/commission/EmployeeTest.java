0b1a0de456ce7754778dc8aebc35855289909e61
popsoft01 <popoolatunde52@gmail.com>
2021-06-25 01:09:53 +0100

    new push

--- TEST/com/ChapterNine/commission/EmployeeTest.java
+++ TEST/com/ChapterNine/commission/EmployeeTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class EmployeeTest {
     Employee employee;
+    CommissionEmployee commision = new CommissionEmployee();
 
     @BeforeEach
     void startWith(){
