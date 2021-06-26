97da238a34f1eb4f51bb2ca34c110f43c843ffe0
popsoft01 <popoolatunde52@gmail.com>
2021-06-26 13:20:34 +0100

    new push

--- TEST/com/ChapterNine/commission/BasePlusCommissionEmployeeTest.java
+++ TEST/com/ChapterNine/commission/BasePlusCommissionEmployeeTest.java
@@ -1,4 +1,12 @@
 package com.ChapterNine.commission;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class BasePlusCommissionEmployeeTest {
+    BasePlusCommissionEmployee base;
+    @BeforeEach
+    void startEachWith(){
+        base = new BasePlusCommissionEmployee();
+    }
 }
