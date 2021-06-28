9f3a3aa63c0086cc1af1c7f3f4b828f320e434fc
popsoft01 <popoolatunde52@gmail.com>
2021-06-28 01:20:37 +0100

    new push

--- src/com/ChapterNine/commission/Circle.java
+++ src/com/ChapterNine/commission/Circle.java
@@ -1,21 +1,21 @@
 package com.ChapterNine.commission;
 
-public class Circle {
-    private boolean filled;
-    private String colour;
+public class Circle extends GeometricObject{
+
     private double radius;
 
     public Circle(double radius) {
+        super();
         this.radius = radius;
     }
 
     public Circle(int radius, String colour, boolean filled) {
+        super(colour,filled);
         this.radius = radius;
-        this.colour = colour;
-        this.filled = filled;
+
     }
 
     public Circle() {
-
+        super();
     }
 }
