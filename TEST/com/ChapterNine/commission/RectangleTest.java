cad26b682ba24c9ff21d71f167b953d8f8796b2a
popsoft01 <popoolatunde52@gmail.com>
2021-06-28 04:49:33 +0100

    new push

---
+++ TEST/com/ChapterNine/commission/RectangleTest.java
@@ -0,0 +1,7 @@
+package com.ChapterNine.commission;
+
+public class RectangleTest {
+    Rectangle shape = new Rectangle();
+    Rectangle shape1 = new Rectangle(6.0,3.0);
+    Rectangle shape2 = new Rectangle(6.0,3.0,"Red",true);
+}
---
+++ src/com/ChapterNine/commission/Rectangle.java
@@ -0,0 +1,13 @@
+package com.ChapterNine.commission;
+
+public class Rectangle extends GeometricObject{
+    public Rectangle(double width, double height) {
+    }
+
+    public Rectangle() {
+
+    }
+
+    public Rectangle(double width, double height, String colour, boolean filled) {
+    }
+}
