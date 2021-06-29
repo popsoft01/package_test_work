664b454ca9f04b6f979d0e213b3cc1244463181a
popsoft01 <popoolatunde52@gmail.com>
2021-06-29 06:16:37 +0100

    new push

--- TEST/com/ChapterTen/Payroll/PayrollSystemModificationTest.java
+++ TEST/com/ChapterTen/Payroll/PayrollSystemModificationTest.java
@@ -1,5 +1,28 @@
 package com.ChapterTen.Payroll;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PayrollSystemModificationTest {
-    WeeklySalary weeklySalary = new WeeklySalary("Olu","Sola","123456");
+    WeeklySalary weeklySalary; //= new WeeklySalary("Olu","Sola","123456");
+
+    @BeforeEach
+    void startEachWith(){
+        weeklySalary = new WeeklySalary("Olu","Sola","123456");
+    }
+    @Test
+    void canSetFirstname(){
+        assertEquals("Olu",weeklySalary.getFirstName());
+    }
+    @Test
+    void cansetLastName(){
+        assertEquals("Sola",weeklySalary.getLastName());
+    }
+    @Test
+    void canSetSSN(){
+        assertEquals("123456",weeklySalary.getSSN());
+    }
+
 }
--- src/com/ChapterTen/Payroll/Employee.java
+++ src/com/ChapterTen/Payroll/Employee.java
@@ -2,8 +2,26 @@ package com.ChapterTen.Payroll;
 
 public abstract class Employee {
 
+    private final String firstName;
+    private final String lastName;
+    private final String socialSecurityNumber;
+
     public Employee(String firstName, String lastName, String ssn) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        socialSecurityNumber = ssn;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSSN() {
+        return socialSecurityNumber;
+    }
     public abstract double earning();
 }
