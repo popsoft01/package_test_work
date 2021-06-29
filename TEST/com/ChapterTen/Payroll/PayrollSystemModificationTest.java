5856832c302d2896e64f4626b942e9ca97f652b7
popsoft01 <popoolatunde52@gmail.com>
2021-06-29 20:14:03 +0100

    new push

--- TEST/com/ChapterTen/Payroll/PayrollSystemModificationTest.java
+++ TEST/com/ChapterTen/Payroll/PayrollSystemModificationTest.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PayrollSystemModificationTest {
     WeeklySalary weeklySalary; //= new WeeklySalary("Olu","Sola","123456");
 
     @BeforeEach
     void startEachWith(){
-        weeklySalary = new WeeklySalary("Olu","Sola","123456");
+        weeklySalary = new WeeklySalary("Olu","Sola","123456",200.0);
     }
     @Test
     void canSetFirstname(){
@@ -23,6 +24,16 @@ public class PayrollSystemModificationTest {
     @Test
     void canSetSSN(){
         assertEquals("123456",weeklySalary.getSSN());
+    }
+    @Test
+    void canSetWeeklysalary(){
+        weeklySalary.setWeeklySalary(200.0);
+        assertEquals(200.0,weeklySalary.getWeeklySalary());
+    }
+    @Test
+    void setWeeklySalaryCannotBeLessThanZero(){
+       Exception exception = assertThrows(IllegalArgumentException.class,()->weeklySalary.setWeeklySalary(-2),"invalid salary amount");
+        assertEquals("invalid salary amount",exception.getMessage() );
     }
 
 }
--- src/com/ChapterTen/Payroll/WeeklySalary.java
+++ src/com/ChapterTen/Payroll/WeeklySalary.java
@@ -3,12 +3,27 @@ package com.ChapterTen.Payroll;
 
 
 public class WeeklySalary extends Employee {
-    public WeeklySalary(String firstName, String lastName, String SSN) {
+    private double weeklySalary;
+
+    public WeeklySalary(String firstName, String lastName, String SSN, double weeklySalary) {
         super(firstName,lastName,SSN);
+        this.weeklySalary = weeklySalary;
     }
+
+    public double getWeeklySalary() {
+        return weeklySalary;
+    }
+
+    public void setWeeklySalary(double weeklySalary) {
+        if (weeklySalary >0.0){
+        this.weeklySalary = weeklySalary;}
+        else throw new IllegalArgumentException("invalid salary amount");
+    }
+
 
     @Override
     public double earning() {
         return 0;
     }
+
 }
