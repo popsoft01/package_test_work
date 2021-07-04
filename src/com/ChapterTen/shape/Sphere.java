7c4765e8b37ef9dd08dd0484adcfbf38fd6a37d4
popsoft01 <popoolatunde52@gmail.com>
2021-07-04 01:57:30 +0100

    new push

--- src/com/ChapterTen/shape/Sphere.java
+++ src/com/ChapterTen/shape/Sphere.java
@@ -25,11 +25,11 @@ public class Sphere extends  ThreeDymationShape{
 
     @Override
     public double getArea() {
-        return 0;
+        return 4 * Math.PI * Math.pow(radius,2);
     }
 
     @Override
     public double getVolume() {
-        return 0;
+        return (4/3)*Math.PI *Math.pow(radius,3);
     }
 }
