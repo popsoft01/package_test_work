31f128c3d6304d194605686bb305731487c4f65d
popsoft01 <popoolatunde52@gmail.com>
2021-07-07 05:29:55 +0100

    new push

---
+++ TEST/com/ChapterTen/shape/CircleTest.java
@@ -0,0 +1,19 @@
+package com.ChapterTen.shape;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CircleTest {
+    Circle circle;
+
+    @BeforeEach
+    void setUp() {
+        circle = new Circle(2,4,3);
+    }
+    @Test
+    void canCalculateArea(){
+        assertEquals(28.274333882308138,circle.getArea());
+    }
+}
---
+++ src/com/ChapterTen/shape/Circle.java
@@ -0,0 +1,15 @@
+package com.ChapterTen.shape;
+
+public class Circle  extends TwoDemimentionShape{
+    public Circle() {
+    }
+
+    public Circle(double length, double width, double radius) {
+        super(length, width, radius);
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(getRadius(),2);
+    }
+}
