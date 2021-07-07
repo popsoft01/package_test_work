f408a8b94fd15fc4db9cffda6c509b446da9358c
popsoft01 <popoolatunde52@gmail.com>
2021-07-07 05:39:29 +0100

    new push

---
+++ TEST/com/ChapterTen/shape/SquareTest.java
@@ -0,0 +1,12 @@
+package com.ChapterTen.shape;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SquareTest {
+    @Test
+    void canCalculateArae(){
+    Square square = new Square(2,4,2);
+    assertEquals(4,square.getArea());}
+}
---
+++ src/com/ChapterTen/shape/Square.java
@@ -0,0 +1,15 @@
+package com.ChapterTen.shape;
+
+public class Square extends TwoDemimentionShape{
+    public Square() {
+    }
+
+    public Square(double length, double width, double radius) {
+        super(length, width, radius);
+    }
+
+    @Override
+    public double getArea() {
+        return Math.pow(getLength(),2);
+    }
+}
