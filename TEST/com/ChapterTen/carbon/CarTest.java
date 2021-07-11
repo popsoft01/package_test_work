0f444c84c7e2cf78ebf4f9f0b0e73880508a7806
popsoft01 <popoolatunde52@gmail.com>
2021-07-11 18:49:40 +0100

    new push

--- TEST/com/ChapterTen/carbon/CarTest.java
+++ TEST/com/ChapterTen/carbon/CarTest.java
@@ -1,4 +1,11 @@
 package com.ChapterTen.carbon;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class CarTest {
+    Car car;
+    @BeforeEach
+    void startEachWith(){
+        car= new Car();
+    }
 }
--- src/com/ChapterTen/carbon/Car.java
+++ src/com/ChapterTen/carbon/Car.java
@@ -1,6 +1,6 @@
 package com.ChapterTen.carbon;
 
-public class Bicycle implements CarbonFootprint{
+public class Car implements CarbonFootprint{
     @Override
     public void getCarbonFootprint() {
 
