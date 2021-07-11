6f60ea41e57972a83dbcdfa8f3701318f3aefcac
popsoft01 <popoolatunde52@gmail.com>
2021-07-11 20:12:34 +0100

    new push

--- TEST/com/ChapterTen/carbon/CarbonBicycleTest.java
+++ TEST/com/ChapterTen/carbon/CarbonBicycleTest.java
@@ -1,11 +1,19 @@
 package com.ChapterTen.carbon;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CarbonBicycleTest {
     Bicycle bicycle;
     @BeforeEach
     void startEachWith(){
-        bicycle = new Bicycle();
+        bicycle = new Bicycle(12,10);
+    }
+    @Test
+    void can_setSpeed(){
+        bicycle.setSpeed(12);
+        assertEquals(12, bicycle.getSpeed());
     }
 }
--- src/com/ChapterTen/carbon/Bicycle.java
+++ src/com/ChapterTen/carbon/Bicycle.java
@@ -1,8 +1,22 @@
 package com.ChapterTen.carbon;
 
 public class Bicycle implements CarbonFootprint{
+    private double speed;
+
+    public Bicycle(int speed, int gear) {
+
+    }
+
     @Override
     public void getCarbonFootprint() {
 
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getSpeed() {
+        return speed;
     }
 }
