775f14b3879709cdaa03c50a49db1ad0fdabd97d
popsoft01 <popoolatunde52@gmail.com>
2021-07-13 02:01:46 +0100

    new push

--- src/com/chapterFourteen/CompareTos.java
+++ src/com/chapterFourteen/CompareTos.java
@@ -1,8 +1,14 @@
 package com.chapterFourteen;
 
+import java.io.Serializable;
+
 public class CompareTos {
-    public int compareString(String firstString, String secondString){
+    public Serializable compareString(String firstString, String secondString){
     int num = firstString.compareTo(secondString);
+    int n2 = secondString.compareTo(firstString);
+    if (n2 > num){
+        return "first string is greater than second string";
+    }else
 
         return num;
     }
