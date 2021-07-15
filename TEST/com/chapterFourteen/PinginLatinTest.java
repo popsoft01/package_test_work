8ab56136be8bf0c7a39989c20d9a7cef0ca7148f
popsoft01 <popoolatunde52@gmail.com>
2021-07-15 01:56:34 +0100

    String Work

--- TEST/com/chapterFourteen/PinginLatinTest.java
+++ TEST/com/chapterFourteen/PinginLatinTest.java
@@ -1,5 +1,20 @@
 package com.chapterFourteen;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PinginLatinTest {
-    PigLatin pigLatin = new PigLatin();
+    PigLatin pigLatin;
+
+    @BeforeEach
+    void  startEachWith(){
+        pigLatin = new PigLatin();
+    }
+    @Test
+    void sententenceCan_BeSplit(){
+        pigLatin.sentenceToken("OLu is a Boy");
+        assertEquals("OLu",pigLatin.getTokenNizeSentence());
+    }
 }
--- src/com/chapterFourteen/PigLatin.java
+++ src/com/chapterFourteen/PigLatin.java
@@ -2,4 +2,9 @@ package com.chapterFourteen;
 
 public class PigLatin {
 
+    public String getTokenNizeSentence() {
+    }
+
+    public void sentenceToken(String sentence) {
+    }
 }
