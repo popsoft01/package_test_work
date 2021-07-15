a39210447bae8dff856b485f163fc37ba7fe6f32
popsoft01 <popoolatunde52@gmail.com>
2021-07-15 03:35:07 +0100

    String Work

--- src/com/chapterFourteen/PigLatin.java
+++ src/com/chapterFourteen/PigLatin.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public class PigLatin {
 
-    public void sentenceToken(String sentence) {
+    public String[] sentenceToken(String sentence) {
        String[] word = sentence.split(" ");
         System.out.println(Arrays.toString(word));
-        char[] newChar = stringToChar(word);
+
+        return word;
     }
 
     private char[] stringToChar(String[] word) {
@@ -17,5 +18,28 @@ public class PigLatin {
             System.out.println(Arrays.toString(letter));
         }
         return letter;
+    }
+
+    public String moveChatToLast(String[] word){
+        char[] newChar = stringToChar(word);
+        swap(newChar, 0, word.length-1);
+        int i = word.length - 1;
+        while(i > 1) {
+            swap(newChar, i, i-1);
+            i--;
+        }
+        return new String(newChar);
+        }
+    private void swap(char[] arr, int x, int y) {
+        char  c = arr[x];
+        arr[x] = arr[y];
+        arr[y] = c;
+    }
+
+    public static void main(String[] args) {
+        PigLatin pigLatin = new PigLatin();
+        String[] start = pigLatin.sentenceToken("Olu is a Boy");
+        String newd = pigLatin.moveChatToLast(start);
+        System.out.println(newd);
     }
 }
