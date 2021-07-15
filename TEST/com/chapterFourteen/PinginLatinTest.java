320da79fc093f37ad182a7903afbbbee6b441ff5
popsoft01 <popoolatunde52@gmail.com>
2021-07-15 12:12:23 +0100

    String Work

--- TEST/com/chapterFourteen/PinginLatinTest.java
+++ TEST/com/chapterFourteen/PinginLatinTest.java
@@ -1,23 +1,23 @@
-//package com.chapterFourteen;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//public class PinginLatinTest {
-//    PigLatin pigLatin;
-//
-//    @BeforeEach
-//    void  startEachWith(){
-//        pigLatin = new PigLatin();
-//    }
-//    @Test
-//    void sententenceCan_BeSplit(){
-////       pigLatin.sentenceToken("Olu is a Boy");
-////       pigLatin.stringToChar(pigLatin.sentenceToken("Olu is a Boy"));
-////        assertEquals("Olu",pigLatin.getTokenNizeSentence());
-//       String[] newd = pigLatin.sentenceToken("Olu is a Boy");
-//        System.out.println(pigLatin.moveChatToLast(newd));
-//    }
-//}
+package com.chapterFourteen;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PinginLatinTest {
+    PigLatin pigLatin;
+
+    @BeforeEach
+    void  startEachWith(){
+        pigLatin = new PigLatin();
+    }
+    @Test
+    void sententenceCan_BeSplit(){
+       pigLatin.sentenceToken("Olu is a Boy");
+//       pigLatin.stringToChar(pigLatin.sentenceToken("Olu is a Boy"));
+        assertEquals("Olu",pigLatin.getTokenNizeSentence());
+       String[] newd = pigLatin.sentenceToken("Olu is a Boy");
+        System.out.println(pigLatin.moveChatToLast(newd));
+    }
+}
