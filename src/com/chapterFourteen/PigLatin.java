78b91f44a09918c2ab86d30b3f8f18a6c3eedc05
popsoft01 <popoolatunde52@gmail.com>
2021-07-15 08:42:57 +0100

    String Work

--- src/com/chapterFourteen/PigLatin.java
+++ src/com/chapterFourteen/PigLatin.java
@@ -15,31 +15,33 @@ public class PigLatin {
         char[] letter = new char[word.length];
         for (int i = 0; i < word.length; i++){
             letter = word[i].toCharArray();
-            System.out.println(Arrays.toString(letter));
+            swap(letter,0,letter.length-1);
+            System.out.println(letter);
+//            System.out.println(Arrays.toString(letter));
         }
         return letter;
     }
 
-    public String moveChatToLast(String[] word){
-        char[] newChar = stringToChar(word);
-        swap(newChar, 0, word.length-1);
-        int i = word.length - 1;
-        while(i > 1) {
-            swap(newChar, i, i-1);
-            i--;
-        }
-        return new String(newChar);
-        }
-    private void swap(char[] arr, int x, int y) {
-        char  c = arr[x];
-        arr[x] = arr[y];
-        arr[y] = c;
+//    public String moveChatToLast(String[] word){
+//        char[] newChar = stringToChar(word);
+//        swap(newChar, 0, word.length-1);
+//        int i = word.length - 1;
+//        while(i > 1) {
+//            swap(newChar, i, i-1);
+//            i--;
+//        }
+//        return new String(newChar);
+//        }
+    private void swap(char[] arr, int indexToSwap, int expectedIndex) {
+        char character = arr[indexToSwap];
+        arr[indexToSwap] = arr[expectedIndex];
+        arr[expectedIndex] = character;
     }
 
     public static void main(String[] args) {
         PigLatin pigLatin = new PigLatin();
-        String[] start = pigLatin.sentenceToken("Olu is a Boy");
-        String newd = pigLatin.moveChatToLast(start);
+        String[] start = pigLatin.sentenceToken("AM Indexing completed in 28 sec. Shared indexes were applied to 36% of files");
+        char[] newd = pigLatin.stringToChar(start);
         System.out.println(newd);
     }
 }
