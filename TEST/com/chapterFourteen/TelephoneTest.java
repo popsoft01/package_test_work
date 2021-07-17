699cefd61142fd123b4e266fd39dd67d5514790c
popsoft01 <popoolatunde52@gmail.com>
2021-07-17 16:38:08 +0100

    String Work

--- TEST/com/chapterFourteen/TelephoneTest.java
+++ TEST/com/chapterFourteen/TelephoneTest.java
@@ -1,4 +1,14 @@
 package com.chapterFourteen;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class TelephoneTest {
+    Telephone telephone;
+    @BeforeEach
+    void startEach_with(){
+        telephone = new Telephone();
+    }
+    @Test
+
 }
