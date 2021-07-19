5845dd72a7e094d4695b137d19d152807ecf636b
popsoft01 <popoolatunde52@gmail.com>
2021-07-19 22:06:46 +0100

    collection Work

--- src/com/chapterSixteen/Arraylist.java
+++ src/com/chapterSixteen/Arraylist.java
@@ -1,5 +1,20 @@
 package com.chapterSixteen;
 
-public class Arraylist {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class Arraylist {
+    public static void main(String[] args) {
+        String[] colours ={"MAGENTA", "RED", "WHITE", "BLUE", "CYAN"};
+        List<String[]> list = new ArrayList<>();
+        for (String string : colours)
+            list.add(colours);
+
+        String[] removeColors = {"RED", "WHITE", "BLUE"};
+        List<String> removeList = new ArrayList<>();
+            removeList.addAll(Arrays.asList(removeColors));
+
+        System.out.println("ArrayList");
+    }
 }
