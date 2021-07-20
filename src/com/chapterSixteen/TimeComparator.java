538c3b528c6787ea8f4816e7f5d353cc8bc017f3
popsoft01 <popoolatunde52@gmail.com>
2021-07-20 08:20:39 +0100

    collection Work

---
+++ src/com/chapterSixteen/TimeComparator.java
@@ -0,0 +1,23 @@
+package com.chapterSixteen;
+
+
+
+
+import com.ChapterEight.TimeInternal;
+
+import java.util.Comparator;
+
+public class TimeComparator implements Comparator<TimeInternal> {
+    @Override
+    public int compare(TimeInternal time1, TimeInternal time2) {
+        int hourDifference = time1.getHour() - time2.getHour();
+        if (hourDifference != 0)
+            return hourDifference;
+
+        int minuteDifference = time1.getHour() - time2.getHour();
+        if (minuteDifference != 0)
+            return minuteDifference;
+
+        return time1.getSecond() - time2.getSecond();
+    }
+}
