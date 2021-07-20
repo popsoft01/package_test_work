b1c5db8783be3d37c900f8bf0b3157cd94ed8e0c
popsoft01 <popoolatunde52@gmail.com>
2021-07-20 08:48:40 +0100

    collection Work

---
+++ src/com/chapterSixteen/Card.java
@@ -0,0 +1,25 @@
+package com.chapterSixteen;
+
+public class Card {
+    private final Faces faces;
+    private final Suit suit;
+
+
+    public Card(Faces faces, Suit suit) {
+        this.faces = faces;
+        this.suit = suit;
+    }
+
+    public Faces getFaces(){
+        return faces;
+    }
+
+    public Suit getSuit(){
+        return suit;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s of %s", getFaces(),getSuit());
+    }
+}
