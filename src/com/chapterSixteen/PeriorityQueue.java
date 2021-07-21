e45ce1957a9179f29d9ff660336fe810e782f306
popsoft01 <popoolatunde52@gmail.com>
2021-07-21 23:51:26 +0100

    lamdas Work

---
+++ src/com/chapterSixteen/PeriorityQueue.java
@@ -0,0 +1,22 @@
+package com.chapterSixteen;
+
+import java.util.PriorityQueue;
+
+public class PeriorityQueue {
+
+    public static void main(String[] args) {
+        PriorityQueue<Double> list = new PriorityQueue<>();
+        list.offer(20.5);
+        list.offer(12.7);
+        list.add(10.5);
+
+        System.out.print("Polling from queue: ");
+// display elements in queue
+        while ( list.size() > 0)
+        {
+            System.out.printf("%.1f ",list.poll()); //list.peek() ); // view top element
+//            list.poll(); // remove top element
+        }
+    }
+
+}
