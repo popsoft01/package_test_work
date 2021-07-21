46e687ed6655256062683376edeeeb3615332559
popsoft01 <popoolatunde52@gmail.com>
2021-07-21 23:52:36 +0100

    lamdas Work

--- src/com/ChapterSeventeen/LamdasRead.java
+++ src/com/ChapterSeventeen/LamdasRead.java
@@ -7,9 +7,13 @@ public class LamdasRead {
     public static void main(String[] args) {
         int[] values = {3, 10, 6, 1, 4, 8, 2, 5, 9, 7};
 
+        printStream(values);
+
+    }
+
+    private static void printStream(int[] values) {
         System.out.println("Orignal values: ");
         IntStream.of(values).forEach(element -> System.out.printf("%d ",element));
-
     }
 }
 
