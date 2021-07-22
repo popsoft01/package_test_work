06752114f74798de5b749dd04b34344a099e49a3
popsoft01 <popoolatunde52@gmail.com>
2021-07-22 18:07:49 +0100

    lamdas Work

--- src/com/ChapterSeventeen/Emloyeemain.java
+++ src/com/ChapterSeventeen/Emloyeemain.java
@@ -1,7 +1,10 @@
 package com.ChapterSeventeen;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Emloyeemain {
     public static void main(String[] args) {
@@ -15,5 +18,26 @@ public class Emloyeemain {
                 new Employee("Wendy", "Brown", 4236.4, "Marketing")};
         List<Employee> list = Arrays.asList(employees);
         list.forEach(System.out::println);
+        Predicate<Employee> fourToSIxThousand = t->(t.getSalary() >= 4000 && t.getSalary()<=6000);
+        System.out.printf("%nEmployees earning $4000-$6000 per month sorted by salary:%n");
+        list.stream()
+                .filter(fourToSIxThousand)
+                .sorted(Comparator.comparing(Employee::getSalary))
+                .forEach(System.out::println);
+
+        System.out.printf("%nFirst employee who earns $4000-$6000:%n%s%n",
+                list.stream()
+                .filter(fourToSIxThousand)
+                .findFirst()
+                .get());
+
+        Function<Employee,String> byFirstName = Employee::getFirstName;
+        Function<Employee,String> byLastName = Employee::getLastName;
+
+        Comparator<Employee> lastThenFirst = Comparator.comparing(byLastName).thenComparing(byFirstName);
+        System.out.printf("%nEmployees in ascending order by last name then first:%n");
+        list.stream()
+                .sorted(lastThenFirst)
+                .forEach(System.out::println);
     }
 }
