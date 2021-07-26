703a0e9af25659e050d79caa8a6fc9070004e91b
popsoft01 <popoolatunde52@gmail.com>
2021-07-27 00:05:40 +0100

    lamdas Work

---
+++ src/com/chapterEigteen/Fanabocci.java
@@ -0,0 +1,17 @@
+package com.chapterEigteen;
+
+import java.math.BigInteger;
+
+public class Fanabocci {
+    public static BigInteger fanabocci(BigInteger number){
+        if (number.equals(BigInteger.ZERO) || number.equals(BigInteger.ONE))
+            return number;
+        else{
+            return fanabocci(number.subtract(BigInteger.ONE)).add(fanabocci(number.subtract(BigInteger.TWO)));
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fanabocci(BigInteger.valueOf(20)));
+    }
+}
