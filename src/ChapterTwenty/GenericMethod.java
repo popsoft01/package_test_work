ed8d6b201b16aac1b8e12badc8dfa4b91d7e2c8c
popsoft01 <popoolatunde52@gmail.com>
2021-07-28 12:48:58 +0100

    Generic chapter

--- src/ChapterTwenty/GenericMethod.java
+++ src/ChapterTwenty/GenericMethod.java
@@ -5,15 +5,23 @@ public class GenericMethod {
         Integer[] integerArray = {1, 2, 3, 4, 5, 6};
         Double[] doubleArray = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7};
         Character[] characterArray = {'H', 'E', 'L', 'L', 'O'};
+        printArray(integerArray);
+        printArray(doubleArray);
+        printArray(characterArray);
     }
     public static void printArray(Integer[] inputArray){
         for (Integer element: inputArray)
-            System.out.println(element);
+            System.out.print(element + " ");
         System.out.println();
     }
     public static void printArray(Double[] inputArray){
         for (Double element:inputArray)
-            System.out.println(element);
+            System.out.print(element + " ");
+        System.out.println();
+    }
+    public static void printArray(Character[] inputArray){
+        for (Character element:inputArray)
+            System.out.print(element + " ");
         System.out.println();
     }
 }
