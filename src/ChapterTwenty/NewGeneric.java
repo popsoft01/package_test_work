14e08b4980306beb61fbd8cd078972e23cb65ff1
popsoft01 <popoolatunde52@gmail.com>
2021-07-29 23:18:14 +0100

    Generic chapter

---
+++ src/ChapterTwenty/NewGeneric.java
@@ -0,0 +1,17 @@
+//package ChapterTwenty;
+//
+//public class NewGeneric {
+//    public static void printArray2(T[] inputArray){
+//        for (T element:inputArray)
+//            System.out.print(element + " ");
+//        System.out.println();
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        Integer[] integerArray = {1, 2, 3, 4, 5, 6};
+//        Double[] doubleArray = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7};
+//        Character[] characterArray = {'H', 'E', 'L', 'L', 'O'};
+//        printArray2(integerArray);
+//    }
+//}
