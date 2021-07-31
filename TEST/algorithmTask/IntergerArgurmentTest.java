770b7445eea4ead524a488c22dcb011c465b13f6
popsoft01 <popoolatunde52@gmail.com>
2021-07-31 09:12:59 +0100

    Generic chapter

---
+++ TEST/algorithmTask/IntergerArgurmentTest.java
@@ -0,0 +1,12 @@
+package algorithmTask;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntergerArgurmentTest {
+
+    @Test
+    void sum() {
+    }
+}
---
+++ src/algorithmTask/IntergerArgurment.java
@@ -0,0 +1,23 @@
+package algorithmTask;
+
+import java.util.Arrays;
+
+public class IntergerArgurment {
+    public static int[] sum(int[] a, int b){
+        int[] c = new int[2];
+        for (int i =0; i<a.length; i++){
+            for (int j = i + 1; j <a.length; j++){
+                if (a[i] + a[j]==b) {
+                   c[0]=i;
+                   c[1] = j;
+                }
+            }
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 2, 5, 3, 5};
+        System.out.println(Arrays.toString(sum(array, 8)));
+    }
+}
