efd32353764ca87a14c800735af40711b27461a8
popsoft01 <popoolatunde52@gmail.com>
2021-07-31 17:56:21 +0100

    Generic chapter

--- src/ChapterTwenty/GenericMain.java
+++ src/ChapterTwenty/GenericMain.java
@@ -5,19 +5,23 @@ public class GenericMain {
     public static void main(String[] args) {
 
     }
+
     public void pushDouble(Stack<Double> stacks,double[] array){
         for (double element: array){
             stacks.push(element);
         }
     }
+
     public Double popDouble(Stack<Double> stack) throws EmptyStackException {
         return stack.pop();
     }
+
     public void pushInt(Stack<Integer> stacks,int[] array){
         for (int element: array){
             stacks.push(element);
         }
     }
+
     public int popInt(Stack<Integer> stack) throws EmptyStackException {
         return stack.pop();
     }
