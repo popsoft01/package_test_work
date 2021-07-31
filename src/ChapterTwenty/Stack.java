326c8ebbc85887ea50e4078cc39d7f4fcd44d333
popsoft01 <popoolatunde52@gmail.com>
2021-07-31 17:54:29 +0100

    Generic chapter

--- src/ChapterTwenty/Stack.java
+++ src/ChapterTwenty/Stack.java
@@ -13,14 +13,17 @@ public class Stack<T> {
         int newCapacity = newElement > 0 ? newElement :10;
         element = new ArrayList<>(newCapacity);
     }
+
     public  void push(T item){
         element.add(item);
     }
+
     public T pop() throws EmptyStackException {
         if (element.isEmpty()) // if stack is empty
             throw new EmptyStackException("Stack is empty, cannot pop");
         return element.remove(element.size() - 1);
     }
+
     public int getSize(){
       return element.size();
     }
