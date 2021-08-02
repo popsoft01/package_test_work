e1bc7eb26ef8cf8332e112cb7a7987b8794f30d6
popsoft01 <popoolatunde52@gmail.com>
2021-08-02 23:10:27 +0100

    Generic chapter

---
+++ src/com/ChapterEight/Course.java
@@ -0,0 +1,11 @@
+package com.ChapterEight;
+
+public class Course {
+    private String courseName;
+    private String[] student;
+    private int numberOfStudents;
+
+    public Course(String courseName){
+        this.courseName = courseName;
+    }
+}
