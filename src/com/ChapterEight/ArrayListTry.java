a6a157f9b058a5849e50e455d3433cc61e8b3023
popsoft01 <popoolatunde52@gmail.com>
2021-08-04 21:28:02 +0100

    Generic chapter

---
+++ src/com/ChapterEight/ArrayListTry.java
@@ -0,0 +1,24 @@
+package com.ChapterEight;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListTry {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = 0;
+        ArrayList<Integer> list = new ArrayList<>(10);
+        do {
+            System.out.println("Enter a unique number");
+            number = input.nextInt();
+
+            if (!list.contains(number) && number != 0){
+                list.add(number);
+            }
+
+        }while (number != 0);
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
+        }
+    }
+}
