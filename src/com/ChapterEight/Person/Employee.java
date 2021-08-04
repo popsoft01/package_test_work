b5d0c8b6713c212a3d8873199be83201e7b329f0
popsoft01 <popoolatunde52@gmail.com>
2021-08-04 22:47:02 +0100

    Generic chapter

--- src/com/ChapterEight/Person/Employee.java
+++ src/com/ChapterEight/Person/Employee.java
@@ -1,4 +1,34 @@
 package com.ChapterEight.Person;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Employee extends Person{
+    private String officeName;
+    private BigDecimal salary;
+    private Date date;
+
+    public String getOfficeName() {
+        return officeName;
+    }
+
+    public void setOfficeName(String officeName) {
+        this.officeName = officeName;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
