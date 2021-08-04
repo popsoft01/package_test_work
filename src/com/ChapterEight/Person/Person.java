8c09570af21701470e1c77f4f02399066b238258
popsoft01 <popoolatunde52@gmail.com>
2021-08-04 22:40:59 +0100

    Generic chapter

--- src/com/ChapterEight/Person/Person.java
+++ src/com/ChapterEight/Person/Person.java
@@ -1,5 +1,45 @@
 package com.ChapterEight.Person;
 
 public class Person {
+private String name;
+private String address;
+private String PhoneNumber;
+private enum status{freshman, sophomore, junior,  senior};
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+
 
 }
