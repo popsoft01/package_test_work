2d7792bb7cc22d5ad11c25476772e87c3bf3d66e
popsoft01 <popoolatunde52@gmail.com>
2021-08-04 18:26:43 +0100

    Generic chapter

--- src/com/ChapterEight/StackClass.java
+++ src/com/ChapterEight/StackClass.java
@@ -11,4 +11,25 @@ public class StackClass {
     public StackClass(int capacity){
         element = new int[capacity];
     }
+    public void push(int value){
+        if (size >= element.length){
+            int[] temp = new int[element.length * 2];
+            System.arraycopy(element,0,temp,0,element.length);
+            element = temp;
+        }
+        element[++size] = value;
+    }
+    public int pop(){
+      return element[--size];
+    }
+    public int peek(){
+        return element[size-1];
+    }
+
+    public  boolean isEmpty(){
+        return size == 0;
+    }
+    public int getSize(){
+        return size;
+    }
 }
