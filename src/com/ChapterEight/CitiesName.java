b523fb378e0cec30345f333b835bcbd4b23eeb18
popsoft01 <popoolatunde52@gmail.com>
2021-08-06 15:36:31 +0100

    Generic chapter

---
+++ src/com/ChapterEight/CitiesName.java
@@ -0,0 +1,24 @@
+package com.ChapterEight;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Locale;
+
+public class CitiesName {
+    public static void main(String[] args) {
+        ArrayList<String> collection = new ArrayList<>();
+        collection.add("Lara");
+        collection.add("Bola");
+        collection.add("titi");
+        collection.add("Grace");
+        collection.add("Dist");
+
+        Iterator<String> iterator = collection.iterator();
+        while(iterator.hasNext()){
+            System.out.print(iterator.next().toUpperCase(Locale.ROOT) + " ");
+        }
+
+
+    }
+}
