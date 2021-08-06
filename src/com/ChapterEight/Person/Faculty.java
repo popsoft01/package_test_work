59f1ddcf2189183c1132f08e4228cd95cdc66c16
popsoft01 <popoolatunde52@gmail.com>
2021-08-06 14:38:26 +0100

    Generic chapter

--- src/com/ChapterEight/Person/Faculty.java
+++ src/com/ChapterEight/Person/Faculty.java
@@ -1,4 +1,30 @@
 package com.ChapterEight.Person;
 
 public class Faculty extends Employee{
+    private String title;
+    private String rank;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty{" +
+                "title='" + title + '\'' +
+                ", rank='" + rank + '\'' +
+                '}';
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
 }
