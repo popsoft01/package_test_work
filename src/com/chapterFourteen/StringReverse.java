c21a03abd3cd0cc86c7ca8583888afab0b35d591
popsoft01 <popoolatunde52@gmail.com>
2021-08-09 17:39:10 +0100

    Generic chapter

---
+++ src/com/chapterFourteen/StringReverse.java
@@ -0,0 +1,21 @@
+package com.chapterFourteen;
+
+
+public class StringReverse {
+    public static void reverseStringToken(String sentence){
+        String[] words = sentence.split(" ");
+        StringBuilder stringBuilder = null;
+        for (String word : words){
+            stringBuilder = new StringBuilder(word);
+            System.out.print(stringBuilder.reverse() + " ");}
+
+    }
+
+//    public static StringBuilder reverseWord(String[] words){
+//
+//    }
+
+    public static void main(String[] args) {
+        reverseStringToken("Write an application that inputs a line of text and outputs the text twice—once in all uppercase letters and once in all lowercase letters.");
+    }
+}
