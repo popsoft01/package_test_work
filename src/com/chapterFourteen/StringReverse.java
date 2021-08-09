d5a60c30b1d204b7c8a7d9fef610624cb3957ef0
popsoft01 <popoolatunde52@gmail.com>
2021-08-09 17:57:01 +0100

    Generic chapter

--- src/com/chapterFourteen/StringReverse.java
+++ src/com/chapterFourteen/StringReverse.java
@@ -1,14 +1,22 @@
 package com.chapterFourteen;
 
 
+import java.util.Locale;
+
 public class StringReverse {
     public static void reverseStringToken(String sentence){
         String[] words = sentence.split(" ");
         StringBuilder stringBuilder;
         for (String word : words){
             stringBuilder = new StringBuilder(word);
-            System.out.print(stringBuilder.reverse() + " ");}
+            System.out.print(stringBuilder.reverse() + " ");
+        }
+        System.out.println();
+    }
 
+    public static void upperCaseAndLowerCase(String input){
+        System.out.println(input.toUpperCase());
+        System.out.println(input.toLowerCase());
     }
 
 //    public static StringBuilder reverseWord(String[] words){
@@ -16,6 +24,8 @@ public class StringReverse {
 //    }
 
     public static void main(String[] args) {
-        reverseStringToken("Write an application that inputs a line of text and outputs the text twice—once in all uppercase letters and once in all lowercase letters.");
+        String input = "Write an application that inputs a line of text and outputs the text twice—once in all uppercase letters and once in all lowercase letters.";
+        reverseStringToken(input);
+        upperCaseAndLowerCase(input);
     }
 }
