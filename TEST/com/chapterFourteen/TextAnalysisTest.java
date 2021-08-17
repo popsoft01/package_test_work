3ccd966e9573db965a7f0abe91fe904e4ffe620a
popsoft01 <popoolatunde52@gmail.com>
2021-08-17 02:41:30 +0100

    Generic chapter

---
+++ TEST/com/chapterFourteen/TextAnalysisTest.java
@@ -0,0 +1,21 @@
+package com.chapterFourteen;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.BooleanSupplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextAnalysisTest {
+
+    @BeforeEach
+    void setup(){
+
+    }
+    @Test
+    void haveAnalysisTestRepository(){
+
+    }
+
+}
