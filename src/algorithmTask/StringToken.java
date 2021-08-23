942e79156a4db435b1f9002becee629b12cbec64
popsoft01 <popoolatunde52@gmail.com>
2021-08-23 15:44:53 +0100

    Generic chapter

---
+++ src/algorithmTask/StringToken.java
@@ -0,0 +1,22 @@
+package algorithmTask;
+
+public class StringToken {
+    public static void stringSplit(String input){
+        String symbol = "[!,?._'@\\s]+";
+        if (input.trim().length()==0 || input.trim().length()>400000)
+        {
+            System.out.println(0);
+        }else{
+        String[] newString = input.trim().split(symbol);
+        System.out.println(newString.length);
+        for(String word : newString){
+            System.out.println(word);
+        }
+    }
+    }
+
+
+    public static void main(String[] args) {
+        stringSplit(" He is a very very good boy, isn't he?");
+    }
+}
