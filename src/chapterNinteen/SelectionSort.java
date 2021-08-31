0fb45c3ad2be60d1b5621d51a3a11dd61f3e919a
popsoft01 <popoolatunde52@gmail.com>
2021-08-31 16:15:53 +0100

    Generic chapter

---
+++ src/chapterNinteen/SelectionSort.java
@@ -0,0 +1,34 @@
+package chapterNinteen;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class SelectionSort<T> {
+    public static <T extends Comparable<T>> T[] sort(T[] array) {
+
+        for (int j = 0; j < array.length; j++) {
+
+
+            for (int i = 0; i < array.length; i++) {
+
+                if (array[j].compareTo(array[i]) > 0) {
+                   T temp = array[i];
+                   array[i] = array[j];
+                   array[j] = temp;
+
+
+                    }
+
+                }
+            }
+        return array;
+        }
+
+
+    public static void main(String[] args) {
+        Integer[] array = {4,10,20,5,7};
+        Integer[] newArray = sort(array);
+        System.out.println(Arrays.toString(newArray));
+    }
+}
+
