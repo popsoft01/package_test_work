24e84dc46631268588e4182b16a0240225ac954c
popsoft01 <popoolatunde52@gmail.com>
2021-09-02 15:37:48 +0100

    generic

---
+++ src/twentyOne/ListNode.java
@@ -0,0 +1,31 @@
+package twentyOne;
+
+public class ListNode<T> {
+    T data;
+    ListNode<T> nextNode;
+
+    public ListNode(T data) {
+        this(data,null);
+    }
+
+    public  ListNode(T object, ListNode<T> nextNode) {
+        data = object;
+        this.nextNode = nextNode;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public ListNode<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(ListNode<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+}
