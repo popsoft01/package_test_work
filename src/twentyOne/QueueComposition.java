5098888da95f284cb568cae0ecf672fc207fc4af
popsoft01 <popoolatunde52@gmail.com>
2021-09-02 16:34:28 +0100

    generic

---
+++ src/twentyOne/QueueComposition.java
@@ -0,0 +1,24 @@
+package twentyOne;
+
+public class QueueComposition<T> {
+    private List<T> queueList;
+
+    public QueueComposition(){
+        queueList = new List<>("queue");
+    }
+
+    public void enqueue(T object){
+        queueList.insertAtBack(object);
+    }
+    public T removeQueue() throws EmptyListException {
+
+     return queueList.removeFromFront();
+    }
+    public boolean isEmpty(){
+        return queueList.isEmpty();
+    }
+    public void print() throws EmptyListException {
+        queueList.print();
+    }
+
+}
