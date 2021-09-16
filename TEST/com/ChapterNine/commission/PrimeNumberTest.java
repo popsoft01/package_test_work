c40104f8d8e92b4f90a5c1689b69a330a7a2c613
popsoft01 <popoolatunde52@gmail.com>
2021-09-16 22:22:27 +0100

    generic

---
+++ TEST/com/ChapterNine/commission/PrimeNumberTest.java
@@ -0,0 +1,24 @@
+package com.ChapterNine.commission;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class PrimeNumberTest {
+    PrimeNumber primeNumber;
+
+    @BeforeEach
+    void setUp(){
+        primeNumber = new  PrimeNumber();
+    }
+    @Test
+    void setPrimeNumberNotNull(){
+        assertNotNull(primeNumber);
+    }
+    @Test
+    void canCheckForPrimeNumber(){
+//        assertEquals();
+    }
+}
