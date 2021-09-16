d3a812825be8b0c68ee615883da7b0d62b2c5848
popsoft01 <popoolatunde52@gmail.com>
2021-09-16 23:54:08 +0100

    generic

--- TEST/algorithmTask/InsertionSortTest.java
+++ TEST/algorithmTask/InsertionSortTest.java
@@ -17,10 +17,24 @@ public class InsertionSortTest {
         }
         return array;
     }
+    public static int[] bubbleSort(int[] list) {
+        for (int k = 1; k < list.length; k++) {
+// Perform the kth pass
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    int temp = list[i + 1];
+                    list[i + 1] = list[i];
+                    list[i] = temp;
+                }
+            }
 
+        }
+        return list;
+    }
     public static void main(String[] args) {
         int[] list = {1, 9, 4, 6, 5, -4};
-        System.out.println(Arrays.toString(insertSort(list)));
+        System.out.println(Arrays.toString(bubbleSort(list)));
+//        System.out.println(Arrays.toString(insertSort(list)));
     }
 
 }
