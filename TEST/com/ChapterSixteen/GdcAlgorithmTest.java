41abdaec01b26d7e82d403f1283fb4901c89547a
popsoft01 <popoolatunde52@gmail.com>
2021-09-16 16:52:31 +0100

    generic

--- TEST/com/ChapterSixteen/GdcAlgorithmTest.java
+++ TEST/com/ChapterSixteen/GdcAlgorithmTest.java
@@ -3,8 +3,8 @@ package com.ChapterSixteen;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static com.ChapterSixteen.GdcAlgorithm.gdc;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GdcAlgorithmTest {
 
@@ -21,14 +21,27 @@ public class GdcAlgorithmTest {
     }
     @Test
     void  gdcCanbecalculated(){
-        int numReturn = GdcAlgorithm.gdc(8,12);
+        int numReturn = gdc(8,12);
         assertEquals(4, numReturn);
     }
 
     @Test
     void firstNumberGreaterThanSecondNumber(){
-        int numReturn = GdcAlgorithm.gdc(16,12);
+        int numReturn = gdc(16,12);
         assertEquals(4, numReturn);
+    }
+    @Test
+    void checkForZeroInFirstNumber(){
+//        int numReturn = GdcAlgorithm.gdc(0,12);
+        Exception exception = assertThrows(IllegalArgumentException.class, ()->gdc(0,12),"Zero is indivisible");
+        assertEquals(exception.getMessage(), "Zero is indivisible");
+    }
+
+    @Test
+    void checkForZeroInSecondNumber(){
+//        int numReturn = GdcAlgorithm.gdc(0,12);
+        Exception exception = assertThrows(IllegalArgumentException.class, ()-> gdc(12,0),"Zero is indivisible");
+        assertEquals(exception.getMessage(), "Zero is indivisible");
     }
 
 
--- src/com/ChapterSixteen/GdcAlgorithm.java
+++ src/com/ChapterSixteen/GdcAlgorithm.java
@@ -3,6 +3,9 @@ package com.ChapterSixteen;
 public class GdcAlgorithm {
     public static int gdc(int firstNumber,int secondNumber) {
 //        int i = firstNumber / 2;
+        if (firstNumber == 0 || secondNumber == 0){
+            throw new IllegalArgumentException("Zero is indivisible");
+        }
         int gdc = 0;
         for (int i = 1; i <= firstNumber/2; i++){
             if (firstNumber% i ==0 && secondNumber% i == 0){
