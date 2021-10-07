07bec01b5e31620155e95bbd506c4e5a43181a25
popsoft01 <popoolatunde52@gmail.com>
2021-10-07 01:16:43 +0100

    generic

---
+++ src/algorithmTask/CreditCardValidation.java
@@ -0,0 +1,43 @@
+package algorithmTask;
+
+import java.util.Arrays;
+
+public class CreditCardValidation {
+    public int[] cardValidation(String cardNumber) {
+        int length = cardNumber.length();
+        int[] cardNumberArray = new int[length];
+        for (int i = 0; i < cardNumber.length(); i++) {
+            cardNumberArray[i] = Integer.parseInt(cardNumber.substring(i, i + 1));
+        }
+//        System.out.println(Arrays.toString(c));
+
+        return cardNumberArray;
+    }
+
+    public int[] multiplyCardNumberFromBack(int[] stringArray) {
+        int multiply = 0;
+        for (int j = stringArray.length - 1; j > 0; j = j - 2) {
+            multiply = stringArray[j] * 2;
+            if (multiply > 9) {
+                multiply = multiply % 10 + 1;
+            }
+            stringArray[j] = multiply;
+//            System.out.println(Arrays.toString(stringArray));
+        }
+        System.out.println(Arrays.toString(stringArray));
+        return stringArray;
+    }
+
+    public int sumTheCardNumber(int[] newString) {
+        int totalSum = 0;
+//        newString = multiplyCardNumberFromBack(newString);
+        for (int j : newString) {
+            totalSum = totalSum + j;
+        }
+        return totalSum;
+    }
+
+    public boolean isValid(int cardSummation) {
+        return cardSummation % 10 == 0;
+    }
+}
