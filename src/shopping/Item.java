4ce20900d230c395131ca658a9f46256961a9a63
popsoft01 <popoolatunde52@gmail.com>
2021-10-07 21:11:44 +0100

    shopping Cart

---
+++ src/shopping/Item.java
@@ -0,0 +1,42 @@
+package shopping;
+
+import java.math.BigInteger;
+
+public class Item {
+    private String id;
+    private BigInteger price;
+    private String description;
+
+    public Item() {
+    }
+
+    public Item(String id, BigInteger price, String description) {
+        this.id = id;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public BigInteger getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigInteger price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
