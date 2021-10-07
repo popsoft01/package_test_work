d5a9aeead317efb2da1d01b0486e13ddaa1b76b7
popsoft01 <popoolatunde52@gmail.com>
2021-10-07 21:18:13 +0100

    shopping Cart

---
+++ src/shopping/Cart.java
@@ -0,0 +1,11 @@
+package shopping;
+
+import twentyOne.List;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class Cart {
+    private static BigInteger totalPrice;
+    private List<Item> itemNumber = new ArrayList<>();
+}
