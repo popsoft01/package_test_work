fe6237eb486c6bd1303938ba21aaaa56d8aecb66
popsoft01 <popoolatunde52@gmail.com>
2021-10-09 17:14:36 +0100

    shopping Cart

---
+++ src/hospitalManagementApplication/Entity/Nurse.java
@@ -0,0 +1,95 @@
+package hospitalManagementApplication.Entity;
+
+import java.time.LocalDate;
+
+public class Nurse {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate dob;
+    private long age;
+    private String password;
+
+    public Nurse() {
+    }
+
+    public Nurse(long id, String firstName, String lastName, String email, LocalDate dob, long age, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public long getAge() {
+        return age;
+    }
+
+    public void setAge(long age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Nurse{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
