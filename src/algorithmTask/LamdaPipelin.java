4bdb3508b377501994bbf4fb5f1db01b3822b0d1
popsoft01 <popoolatunde52@gmail.com>
2021-10-22 09:18:42 +0100

    hospitall

--- src/algorithmTask/LamdaPipelin.java
+++ src/algorithmTask/LamdaPipelin.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class LamdaPipelin {
     public static void main(String[] args) {
         List<String> myList = Arrays.asList("a1", "a2", "b1", "c2", "c1");
-
         myList.stream()
                 .filter(s -> s.startsWith("c"))
                 .map(String::toUpperCase)
