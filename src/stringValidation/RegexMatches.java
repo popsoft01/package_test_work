1ae8c0414dddc2f325490d92f65f2d32916421c0
popsoft01 <popoolatunde52@gmail.com>
2022-03-06 00:54:54 +0100

    hospitall

---
+++ src/stringValidation/RegexMatches.java
@@ -0,0 +1,20 @@
+package stringValidation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexMatches {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("D.*\\d[0-35-9]-\\d\\d-\\d\\d");
+
+        String string1 = "Jane's Birthday is 05-12-75\n" +
+                "Dave's Birthday is 11-04-68\n" +
+                "John's Birthday is 04-28-73\n" +
+                "Joe's Birthday is 12-17-77";
+
+        Matcher matcher = pattern.matcher(string1);
+        while (matcher.find()){
+            System.out.println(matcher.group());
+        }
+    }
+}
