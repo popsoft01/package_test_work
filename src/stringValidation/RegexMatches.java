909c81546d9471f4aefcb6e95a41b0ef94706f43
popsoft01 <popoolatunde52@gmail.com>
2022-03-12 13:14:02 +0100

    hospitall

--- src/stringValidation/RegexMatches.java
+++ src/stringValidation/RegexMatches.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class RegexMatches {
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("D.*\\d[0-35-9]-\\d\\d-\\d\\d");
+        Pattern pattern = Pattern.compile("D3241.*\\d[0-35-9]-\\d\\d-\\d\\d");
 
         String string1 = "Jane's Birthday is 05-12-75\n" +
                 "Dave's Birthday is 11-04-68\n" +
